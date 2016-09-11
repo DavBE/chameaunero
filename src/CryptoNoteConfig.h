8ab28f16d170285196b58863c9038ce35835c8b5
DavBE <davfrombe@gmail.com>
2016-09-11 21:45:02 +0200

    added second seed node

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -107,7 +107,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "164.132.45.215:14141"
+  "164.132.45.215:14141",
+  "51.254.135.237:14141"
   //"your_seed_ip1.com:8080",
   //"your_seed_ip2.com:8080",
 };
